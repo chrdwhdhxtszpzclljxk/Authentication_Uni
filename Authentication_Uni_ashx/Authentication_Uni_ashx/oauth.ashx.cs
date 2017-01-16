26b1cb8128c267a09d27a642a1248f6d9ab4816f
毛毛 <20774746@qq.com>
2017-01-16 17:43:47 +0800

    ffff
    fffff

--- Authentication_Uni_ashx/Authentication_Uni_ashx/oauth.ashx.cs
+++ Authentication_Uni_ashx/Authentication_Uni_ashx/oauth.ashx.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Script.Serialization;
+
 
 namespace Authentication_Uni_ashx
 {
@@ -14,6 +20,7 @@ namespace Authentication_Uni_ashx
 
         public void ProcessRequest(HttpContext c)
         {
+            string strConnetionString = System.Configuration.ConfigurationManager.ConnectionStrings["gtmasqlserver"].ConnectionString;
             c.Response.ContentType = "application/json; charset=utf-8";
             Dictionary<string, object> dict = new Dictionary<string, object>();
             JavaScriptSerializer serializer = new JavaScriptSerializer();
@@ -34,7 +41,66 @@ namespace Authentication_Uni_ashx
             }
             else if (cmd == "password")
             {
-                
+                string un = c.Request["username"];
+                string pwd = c.Request["password"].Substring(8,16);
+                string userguid = "";
+                //AuthorizationWithAppguid
+                dict["msg"] = "用户名或密码错误";
+                try
+                {
+                    SqlConnection sc = new SqlConnection(strConnetionString);
+                    SqlCommand sm = new SqlCommand();
+                    sm.CommandType = CommandType.StoredProcedure;
+                    sm.CommandText = "[AuthorizationWithAppguid]";
+                    sm.Connection = sc;
+                    sm.Parameters.Add(new SqlParameter("@un", un));
+                    sm.Parameters.Add(new SqlParameter("@pwd", pwd));
+                    sc.Open();
+                    SqlDataReader odr = sm.ExecuteReader();
+                    if (odr.HasRows)
+                    {
+                        byte[] solt = Encoding.Default.GetBytes(Guid.NewGuid().ToString().Substring(0, 8));
+                        string access_token = "";
+                        string refresh_token = "";
+                        string jwt_header = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}";
+                        string jwt_payload = "";
+                        
+                        while (odr.Read())
+                        {
+                            userguid = odr["userguid"].ToString();
+                            jwt_payload = "{";
+                            jwt_payload += "\"id\":\""+ userguid + "\",";
+                            jwt_payload += "\"un\":\"" + un + "\",";
+                            jwt_payload += "\"mg\":\"" + odr["mgroupid"].ToString() + "\"";
+                            jwt_payload += "}";
+                        }
+
+                        byte[] bytes = Encoding.Default.GetBytes(jwt_header);
+                        jwt_header = Convert.ToBase64String(bytes);
+
+                        bytes = Encoding.Default.GetBytes(jwt_payload);
+                        jwt_payload = Convert.ToBase64String(bytes);
+                        HMACSHA256 hmac = new HMACSHA256(solt);
+                        access_token = jwt_header + "." + jwt_payload;
+                        bytes = hmac.ComputeHash(Encoding.Default.GetBytes((access_token)));
+                        access_token += "." + Convert.ToBase64String(bytes);
+
+
+                        dict["access_token"] = access_token;
+                        dict["token_type"] = "bearer";
+                        dict["expires_in"] = 60 * 60 * 2;
+                        dict["refresh_token"] = refresh_token;
+                        dict["msg"] = "用户登录成功";
+                        HttpRuntime.Cache.Insert(userguid, solt, null, System.Web.Caching.Cache.NoAbsoluteExpiration, TimeSpan.FromMinutes(2));
+                    }
+                    odr.Close();
+                    sc.Close();
+                }
+                catch (Exception e)
+                {
+                    dict["msg"] = "错误：" + e.Message;
+                }
+
             }
             else
             {
@@ -50,5 +116,9 @@ namespace Authentication_Uni_ashx
                 return false;
             }
         }
+    }
+
+    public class jwtpkg{
+
     }
 }
